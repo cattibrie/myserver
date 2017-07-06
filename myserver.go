fd3d7975998f5049a3f310040731006a6e8b36a4
cattibrie <tyurina.katty@yandex.ru>
2017-07-06 22:56:36 +0300

    add /set method

--- myserver.go
+++ myserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"sync/atomic"
 )
 
@@ -30,6 +31,22 @@ func resHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		nv := atomic.LoadInt64(&v)
 		fmt.Fprintf(w, "Value v = %d", nv)
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func setHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		sval := r.URL.Query().Get("value")
+		val, err := strconv.ParseInt(sval, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Invalid value argument: %v", err)
+			return
+		}
+		atomic.StoreInt64(&v, val)
+		return
 	}
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
@@ -38,5 +55,6 @@ func main() {
 	http.HandleFunc("/add", addHandler)
 	http.HandleFunc("/dec", decHandler)
 	http.HandleFunc("/result", resHandler)
+	http.HandleFunc("/set", setHandler)
 	http.ListenAndServe(":8080", nil)
 }
