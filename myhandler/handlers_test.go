26bc1f989eceb4ec1ed32ffc91e13454bb1fcf5d
cattibrie <tyurina.katty@yandex.ru>
2017-09-05 23:50:29 +0300

    tests var2

--- myhandler/handlers_test.go
+++ myhandler/handlers_test.go
@@ -6,111 +6,92 @@ import (
 	"testing"
 )
 
-//var targets = []string{
-//"http://localhost:8080/result",
-//"http://localhost:8080/add",
-//"http://localhost:8080/dec",
-//"http://localhost:8080/set?value=20",
-//"http://localhost:8080/set?value=monkey",
-//}
-
-var tests = []string{
-	http.MethodGet,
-	http.MethodPost,
+func initials() (v *Value, h *Handlers) {
+	v = &Value{V: 0}
+	var i Operat = v
+	h = &Handlers{H: i}
+	return v, h
 }
 
-//func addTest
-
-func TestHandlers(t *testing.T) {
-	var v = Value{V: 0}
-	var i Operat = &v
-	var h = Handlers{H: i}
-	for _, m := range tests {
-		//for _, target := range targets {
-		req1 := httptest.NewRequest(m, "http://localhost:8080/add", nil)
-		nv1 := v.V + 1
-		wAdd := httptest.NewRecorder()
-		h.AddHandler(wAdd, req1)
-		respAdd := wAdd.Result()
-		if respAdd.StatusCode != http.StatusOK && respAdd.StatusCode != http.StatusMethodNotAllowed {
-			t.Error(
-				"For", req1,
-				"expected", http.StatusOK, "or", http.StatusMethodNotAllowed,
-				"got", respAdd.StatusCode,
-			)
-		} else if respAdd.StatusCode == http.StatusOK && v.V != nv1 {
-			t.Error(
-				"For value",
-				"expected", nv1,
-				"got", v.V,
-			)
-		}
-
-		req2 := httptest.NewRequest(m, "http://localhost:8080/dec", nil)
-		nv2 := v.V - 1
-		wDec := httptest.NewRecorder()
-		h.DecHandler(wDec, req2)
-		respDec := wDec.Result()
-		if respDec.StatusCode != http.StatusOK && respDec.StatusCode != http.StatusMethodNotAllowed {
-			t.Error(
-				"For", req2,
-				"expected", http.StatusOK, "or", http.StatusMethodNotAllowed,
-				"got", respDec.StatusCode,
-			)
-		} else if respDec.StatusCode == http.StatusOK && v.V != nv2 {
-			t.Error(
-				"For value",
-				"expected", nv2,
-				"got", v.V,
-			)
-		}
-
-		req3 := httptest.NewRequest(m, "http://localhost:8080/result", nil)
-		nv3 := v.V
-		wRes := httptest.NewRecorder()
-		h.ResHandler(wRes, req3)
-		respRes := wRes.Result()
-		if respRes.StatusCode != http.StatusOK && respRes.StatusCode != http.StatusMethodNotAllowed {
-			t.Error(
-				"For", req3,
-				"expected", http.StatusOK, "or", http.StatusMethodNotAllowed,
-				"got", respRes.StatusCode,
-			)
-		} else if respRes.StatusCode == http.StatusOK && v.V != nv3 {
-			t.Error(
-				"For value",
-				"expected", nv3,
-				"got", v.V,
-			)
-		}
-
-		req4 := httptest.NewRequest(m, "http://localhost:8080/set?value=20", nil)
-		var nv4 int64 = 20
-		wSet := httptest.NewRecorder()
-		h.SetHandler(wSet, req4)
-		respSet := wSet.Result()
-		if respSet.StatusCode != http.StatusOK && respSet.StatusCode != http.StatusMethodNotAllowed && respSet.StatusCode != http.StatusBadRequest {
-			t.Error(
-				"For", req4,
-				"expected", http.StatusOK, "or", http.StatusMethodNotAllowed, "or",
-				http.StatusBadRequest,
-				"got", respSet.StatusCode,
-			)
-		} else if respSet.StatusCode == http.StatusOK && v.V != nv4 {
-			t.Error(
-				"For value",
-				"expected", nv4,
-				"got", v.V,
-			)
-		}
-
+func checkhandlers(resp *http.Response, r *http.Request, t *testing.T, v *Value, vexp int64) {
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Error("For", r,
+			"expected", http.StatusOK, "or", http.StatusMethodNotAllowed,
+			"got", resp.StatusCode)
+	} else if resp.StatusCode == http.StatusOK && v.V != vexp {
+		t.Error("For value",
+			"expected", vexp,
+			"got", v.V)
 	}
 }
 
-//func TestAddHandler(t *testing.T)
+func checksethandler(resp *http.Response, r *http.Request, t *testing.T, v *Value, vexp int64) {
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusMethodNotAllowed && resp.StatusCode != http.StatusBadRequest {
+		t.Error("For", r,
+			"expected", http.StatusOK, "or", http.StatusMethodNotAllowed, "or",
+			http.StatusBadRequest, "got", resp.StatusCode)
+	} else if resp.StatusCode == http.StatusOK && v.V != vexp {
+		t.Error("For value",
+			"expected", vexp,
+			"got", v.V)
+	}
+}
 
-//func TestDecHandler(t *testing.T)
+func addhandlerTest(m string, v *Value, t *testing.T, h *Handlers) {
+	req := httptest.NewRequest(m, "http://localhost:8080/add", nil)
+	vexp := v.V + 1
+	w := httptest.NewRecorder()
+	h.AddHandler(w, req)
+	resp := w.Result()
+	checkhandlers(resp, req, t, v, vexp)
+}
 
-//func  TestResHandler(t *testing.T)
+func dechandlerTest(m string, v *Value, t *testing.T, h *Handlers) {
+	req := httptest.NewRequest(m, "http://localhost:8080/dec", nil)
+	vexp := v.V - 1
+	w := httptest.NewRecorder()
+	h.DecHandler(w, req)
+	resp := w.Result()
+	checkhandlers(resp, req, t, v, vexp)
+}
 
-//func TestSetHandler(t *testing.T)
+func reshandlerTest(m string, v *Value, t *testing.T, h *Handlers) {
+	req := httptest.NewRequest(m, "http://localhost:8080/result", nil)
+	vexp := v.V
+	w := httptest.NewRecorder()
+	h.ResHandler(w, req)
+	resp := w.Result()
+	checkhandlers(resp, req, t, v, vexp)
+}
+
+func sethandlerTest(req *http.Request, v *Value, vexp int64, t *testing.T, h *Handlers) {
+	w := httptest.NewRecorder()
+	h.SetHandler(w, req)
+	resp := w.Result()
+	checksethandler(resp, req, t, v, vexp)
+}
+
+func TestHandlersGet(t *testing.T) {
+	v, h := initials()
+	m := http.MethodGet
+	addhandlerTest(m, v, t, h)
+	dechandlerTest(m, v, t, h)
+	reshandlerTest(m, v, t, h)
+	setreq1 := httptest.NewRequest(m, "http://localhost:8080/set?value=20", nil)
+	var vexp1 int64 = 20
+	sethandlerTest(setreq1, v, vexp1, t, h)
+}
+
+func TestHandlersPOST(t *testing.T) {
+	v, h := initials()
+	m := http.MethodPost
+	addhandlerTest(m, v, t, h)
+	dechandlerTest(m, v, t, h)
+	reshandlerTest(m, v, t, h)
+	setreq1 := httptest.NewRequest(m, "http://localhost:8080/set?value=20", nil)
+	var vexp1 int64 = 20
+	sethandlerTest(setreq1, v, vexp1, t, h)
+	setreq2 := httptest.NewRequest(m, "http://localhost:8080/set?value=monkey", nil)
+	vexp2 := v.V
+	sethandlerTest(setreq2, v, vexp2, t, h)
+}
